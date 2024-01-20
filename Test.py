4cc4e7bee44a7118bc29a9ed69ccd6949fd8340f
prooguz <141454604+prooguz@users.noreply.github.com>
2024-01-20 13:11:18 +0300

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,3 @@
+import random
+gercekler = ["Messi>Ronaldo", "3 tane kedim var", "En sevdiğim yemek iskender", "En çok gurur uyduğum proje unity projelerim ve futboldaki başarılarım", "En sevdiğim hobi FUTBOL"]
+print(random.choice(gercekler))
